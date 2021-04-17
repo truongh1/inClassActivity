56a6e35977e663cc1e685579255f6b762cc581b1
Hao Truong <56089268+truongh1@users.noreply.github.com>
2021-04-16 22:20:56 -0700

    passwordGen.py done

--- passwordGen.py
+++ passwordGen.py
@@ -3,9 +3,11 @@ def passwordGen(num):
     chars = ['a','b','c','d','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
     password = []
     for i in range(num):
-        x = random.randrange(63)
+        x = random.randrange(62)
         password.append(chars[x])
 
     print("".join(password))
     
 num = input("Enter number: ")
+passwordGen(int(num))
+
